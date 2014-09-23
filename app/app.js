9cf551c425ff5514191e1ca98953e2251aaf60fd
Rares Golea <rgolea@Raress-MacBook-Pro.local>
2014-09-24 01:04:43 +0200

    finished interface | wire backend

---
+++ app/app.js
@@ -0,0 +1,19 @@
+var app = angular.module('app', []);
+
+app.controller('todoCtrl', ['$scope', function($scope){
+
+    $scope.todos = [];
+    
+    $scope.add = function(){
+        $scope.todos.push($scope.newToDo);
+        $scope.newToDo = null;
+    };
+    
+    $scope.delete = function(i){
+        $scope.todos.splice(i, 1);
+    };
+    
+    $scope.edit = function(i){
+        delete $scope.todos[i].edit;
+    };
+}]);
